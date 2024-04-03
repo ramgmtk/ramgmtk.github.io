2a74f66c939b268affdd9e53fa343d7da8d1c861
ramgmtk <ramgmtk@gmail.com>
2024-04-03 15:05:39 -0700

    Preppring for json and js addition

---
+++ src/page_builder.js
@@ -0,0 +1,59 @@
+import pData from './projects.json' assert { type: 'json'};
+//https://marketplace.visualstudio.com/items?itemName=yuichinukiyama.vscode-preview-server
+//we cannot use require as that is specific to a node.js environment
+/*async function fetchProjects() {
+    const pData = await require('./projects.json');
+    return pData;
+}*/
+
+function buildProjectPage(projectData) {
+    const projectDiv = document.getElementById("project_page");
+    projectData.forEach( function (project)  {
+        //create header for project
+        const pDiv = document.createElement('div');
+        pDiv.classList.add('project_header');
+        const pTag = document.createElement('p');
+        const titleNode = document.createTextNode(project.header);
+        pTag.append(titleNode);
+        pDiv.append(pTag);
+        if (Object.hasOwn(project, 'repo_link')) {
+            const aTag = document.createElement('a');
+            aTag.href = project.repo_link;
+            aTag.target = '_blank';
+            const linkTextNode = document.createTextNode(project.repo_title);
+            aTag.append(linkTextNode);
+            pDiv.append(aTag);
+        }
+        projectDiv.append(pDiv);
+        //create project body
+        const pBodyDiv = document.createElement('div');
+        pBodyDiv.classList.add('project');
+        const pBodyTag = document.createElement('p');
+        const textBodyNode = document.createTextNode(project.description);
+        pBodyTag.append(textBodyNode);
+        pBodyDiv.append(pBodyTag);
+        //create link
+        if (Object.hasOwn(project, 'source_type')) {
+            if (project.source_type == 'img') {
+                const imgTag = document.createElement('img');
+                imgTag.src = project.source_link;
+                imgTag.width = project.width;
+                imgTag.height = project.height;
+                pBodyDiv.append(imgTag);
+            } else {
+                const iframeTag = document.createElement('iframe');
+                iframeTag.src = project.source_link;
+                pBodyDiv.append(iframeTag);
+            }
+        }
+        projectDiv.append(pBodyDiv);
+        console.log(project.header);
+    })
+}
+
+function buildPage() {
+    //const pData = await fetchProjects();
+    buildProjectPage(pData.projects);
+}
+
+buildPage();
